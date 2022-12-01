a9b0655c0c51d7b008ab758cb55268975ae65383
EricAning1 <ericaning@hotmail.com>
2022-11-30 21:17:22 -0600

    greeting

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
 let port = 8000;
+
+//GREETINGS
+app.get('/greeting', (req, res) => {
+  res.send('Hello brother!');
+});
+
+app.get('/greeting/:name', (req, res) => {
+  res.send(`Hey, ${req.params.name}!`);
+});
+
+app.listen(port, (req, res) => {
+  console.log(`listening on port ${port}....`);
+});
